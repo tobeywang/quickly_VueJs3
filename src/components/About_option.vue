f40bef56d506683e24f9521d7e3670f5185f1c87
tobeywang <tobey.wang@ctbcinvestments.com>
2025-08-25 16:57:09 +0800

    diff option api and component api

---
+++ src/components/About_option.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2>About 頁面</h2>
+    <p>{{ message }}</p>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  // 特徵1：data 選項
+  data() {
+    return {
+      count: 0,
+      message: 'Hello, Option API !'
+    };
+  },
+  // 特徵2：methods 選項
+  methods: {
+    increment() {
+      // 特徵3：大量使用 'this'
+      this.count++;
+    },
+  },
+  // 特徵4：生命週期選項
+  mounted() {
+    console.log('Component is mounted!');
+  },
+};
+</script>
